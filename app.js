4428ec9fffdc61bf625c4ec5675cd549888dc2f8
Ajin28 <amisha28jain@gmail.com>
2020-09-13 11:25:11 +0530

    Index route

--- app.js
+++ app.js
@@ -3,17 +3,48 @@ const express = require("express"),
     mongoose = require("mongoose"),
     app = express();
 
+//APP CONFIG
 mongoose.connect("mongodb://localhost:27017/BlogSite", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log("Connected to DB!") })
     .catch((err) => { console.log(err) });
-
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-    res.send("working")
+//MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date, default: Date.now()
+    }
+});
+const Blog = mongoose.model("Blog", blogSchema)
+
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1515002246390-7bf7e8f87b54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//     body: "Kedi was such an awesome movie. I think everyone should see it. It was a piece of art from start to finish. Just mesmerizing"
+// });
+
+//RESTFUL ROUTES
+app.get("/", (req, res) => {
+    res.redirect("/blogs")
 })
+
+app.get('/blogs', function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.render("index", { blogs })
+        }
+    })
+});
+
+
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("BlogSite Server has started!");
